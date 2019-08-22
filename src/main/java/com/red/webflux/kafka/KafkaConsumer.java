4d787c58f557c1f8f52cad42d829ef7c476c3e97
Red <243309672@qq.com>
2019-08-22 18:19:48 +0800

    kafka 配置

---
+++ src/main/java/com/red/webflux/kafka/KafkaConsumer.java
@@ -0,0 +1,43 @@
+package com.red.webflux.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+/**
+ * @Author: Red
+ * @Descpription:
+ * @Date: 16:46 2019/8/22
+ */
+@Component
+public class KafkaConsumer<T> {
+
+
+        private Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+
+        /**
+         * 监听kafka.tut 的topic，不做其他业务
+         *
+         * @param record
+         * @param topic  topic
+         */
+        @KafkaListener(id = "tut", topics = "kafka.tut")
+        public void listen(ConsumerRecord<?, ?> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+            Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+
+            if (kafkaMessage.isPresent()) {
+                Object message = kafkaMessage.get();
+                logger.info("Receive： +++++++++++++++ Topic:" + topic);
+                logger.info("Receive： +++++++++++++++ Record:" + record);
+                logger.info("Receive： +++++++++++++++ Message:" + message);
+            }
+        }
+
+    }
+
--- src/test/java/com/red/webflux/webflux/WebfluxApplicationTests.java
+++ src/test/java/com/red/webflux/webflux/WebfluxApplicationTests.java
@@ -1,7 +1,11 @@
 package com.red.webflux.webflux;
 
+import com.red.webflux.kafka.KafkaSender;
+import com.red.webflux.model.Red;
+import org.aspectj.lang.annotation.Around;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +13,29 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class WebfluxApplicationTests {
 
+
+    @Autowired
+    private KafkaSender<Red> kafkaSender;
+
     @Test
-    public void contextLoads() {
+    public void contextLoads()   {
+        //模拟发消息
+        for (int i = 0; i < 5; i++) {
+
+            Red user = new Red();
+            user.setAge(100);
+            user.setMessage("第一次使用kafka");
+            user.setName("kafka");
+
+            kafkaSender.send(user);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
     }
 
 }
